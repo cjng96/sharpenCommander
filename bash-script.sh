83b4965ed476407f9832932a913d10c58853d1a9
FelixChoi <cjng96@gmail.com>
2016-05-18 16:44:57 +0900

    bash: dcf cmd for findg

--- bash-script.sh
+++ bash-script.sh
@@ -3,29 +3,33 @@
 # . $DEV_CMD_PATH/bash-script.sh
 
 
-function dc()
+function goPath()
 {
-	python3 $DEV_CMD_PATH/dc.py $1 $2 $3
-		
 	# goto that path
 	if [ -f /tmp/cmdDevTool.path ]; then
 		DEVPP=$(cat /tmp/cmdDevTool.path)
 		rm -f /tmp/cmdDevTool.path
 		cd $DEVPP
 	fi
+
+}
+function dc()
+{
+	python3 $DEV_CMD_PATH/dc.py "$1" "$2" "$3" "$4" $5 $6 $7 $8 $9
+	goPath
+		
+}
+function dcf()
+{
+	python3 $DEV_CMD_PATH/dc.py findg "$1" "$2"
+	goPath
 }
 
 
 function dcd()
 {
-	python3 -m pudb.run $DEV_CMD_PATH/dc.py $1 $2 $3
-		
-	# goto that path
-	if [ -f /tmp/cmdDevTool.path ]; then
-		DEVPP=$(cat /tmp/cmdDevTool.path)
-		rm -f /tmp/cmdDevTool.path
-		cd $DEVPP
-	fi
+	python3 -m pudb.run $DEV_CMD_PATH/dc.py $1 $2 $3 $4 $5 $6 $7 $8 $9
+	goPath
 }
 
 
