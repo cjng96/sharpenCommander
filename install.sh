1db62ae453e0f60f5cb796670f0d0c5876978a0f
FelixChoi <felix.choi@udptechnology.com>
2018-12-21 16:39:01 +0900

    install: use stable branch for production

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ mkdir -p $TARGET
 cd $TARGET
 
 if [ ! -d devCmdTool ]; then
-    git clone https://github.com/cjng96/devCmdTool.git
+    git clone -b stable https://github.com/cjng96/devCmdTool.git
 else
     echo "There is already devCmdTool repo in /usr/local/src"
 fi
 
-COMMENT="## devCmdTool script"
+COMMENT="## devCmdTool script ##"
 cnt=$(sh -c "grep '$COMMENT' ~/.bashrc | wc -l")
 if [ $cnt -eq  0 ]; then
     echo "Setting up for devCmdTool to ~/.bashrc"
