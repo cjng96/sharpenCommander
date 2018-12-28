b69e797dabb5187bfa33ac4bee11b1e0073661b8
FelixChoi <cjng96@gmail.com>
2018-12-28 14:03:59 +0900

    install: changed git url

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-TARGET=~/.devcmd
+TARGET=~/.synapcmd
 mkdir -p $TARGET
 cd $TARGET
 
 if [[ ! -d devCmdTool ]]; then
-    git clone -b stable https://github.com/cjng96/devCmdTool.git
+    git clone -b stable https://github.com/inertry/synapbookCommander.git
 else
-    echo "There is already devCmdTool repo in $TARGET"
+    echo "There is already SynapbookCommander repo in $TARGET"
 fi
 
-COMMENT="## devCmdTool script ##"
+COMMENT="## SynapbookCommander script ##"
 cnt=$(sh -c "grep '$COMMENT' ~/.bashrc | wc -l")
 if [[ $cnt -eq  0 ]]; then
-    echo "Setting up for devCmdTool to ~/.bashrc"
+    echo "Setting up for SynapbookCommander to ~/.bashrc"
     echo $COMMENT >> ~/.bashrc
     echo ". $TARGET/bash-script.sh" >> ~/.bashrc
     echo "Please type 'dc' after restarting terminal or source ~/.bashrc"
 else
-    echo "Setting is done already. Type 'dc' for starting"
+    echo "Setting is done already. Type 'sc' for starting"
 fi
 
-cd devCmdTool
+cd synapbookCommander
 virtualenv env
 ./env/bin/pip3 install -r requirements.txt
 
