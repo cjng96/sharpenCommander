99378abade1625a8e177183175309f028c3c8573
FelixChoi <cjng96@gmail.com>
2019-03-01 01:09:52 +0900

    install: use python3 for virtualenv

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ cnt=$(sh -c "grep '$COMMENT' ~/.bashrc | wc -l")
 if [[ $cnt -eq  0 ]]; then
     cd synapbookCommander
     [ $? -ne 0 ] && echo "#### no repo folder" && exit
-    virtualenv env
+    virtualenv -p python3 env
     [ $? -ne 0 ] && echo "#### failed to run virtualenv" && exit
     ./env/bin/pip3 install -r requirements.txt
     [ $? -ne 0 ] && echo "#### failed to install python components" && exit
